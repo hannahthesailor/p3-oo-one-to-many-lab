b88c416134ec7cc116a65f724115c4e2af62f8e4
Hannah Bradley <hannahthesailor@yahoo.com>
2024-03-19 15:43:16 -0400

    p3-oo-one-to-many-lab.md

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,32 @@
 class Pet:
-    pass
+
+    all = []
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+   
+    def __init__(self, name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise ValueError(f"Invalid pet type '{pet_type}'. Allowed types are: {', '.join(self.PET_TYPES)}")
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        self.__class__.all.append(self)
+
+        
+
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+       return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Not a valid pet!")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        pets = [pet for pet in Pet.all if isinstance(pet, Pet) and pet.owner == self]
+        return sorted(pets, key=lambda x: x.name)
